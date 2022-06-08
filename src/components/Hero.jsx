412656a30c04aec6132b66a35b1d55ccda3e3212
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-06-08 10:01:08 +0200

    feat: Addiding image elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
                     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-5 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div className="sm:text-center lg:text-left">
                             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span className="block xl:inline">Decorbonizing Our</span>
+                                <span className="block xl:inline">Decarbonizing Our</span>
                                 <span className="block text-indigo-600 xl:inline">Cities</span>
                             </h1>
                             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Fitting for 55 we need to decarbonize European cities until 2050. Stunning green is committed to create an sustainable enviromnent for everyone.</p>
@@ -34,7 +34,7 @@ export default function Hero() {
                 </div>
             </div>
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={heroImage} alt="" />
+                <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1521708266372-b3547456cc2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=838&q=80" alt="" />
             </div>
         </div>
 
